12d05cb7810b0780c85e794cba25ed682cb52797
Hemachandran <hemavikic@gmail.com>
2022-04-09 22:55:05 +0530

    Updated Commit

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const mysql = require("mysql");
+const imageToBase64 = require("image-to-base64");
 
 const app = express();
 
@@ -9,6 +10,36 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 app.use(express.json());
 
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "Hems@1005",
+  database: "sig_db",
+});
+
 app.post("/api/sigimg", function (req, res) {
-  const imageURL = req.body.imageURL;
+  const imageB64 = imageToBase64(req.body.imageURL);
+
+  con.query(
+    "INSERT INTO sig_images (imageURL) VALUES (?)",
+    [imageB64],
+    function (err, result) {
+      if (err) throw err;
+      console.log("1 record inserted");
+    }
+  );
+});
+
+app.get("/api/getimg", function (req, res) {
+  con.query("SELECT * FROM sig_images", (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(result);
+    }
+  });
+});
+
+app.listen(3010, function () {
+  console.log("Server running on Port 3010");
 });
--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import "./sigCanvas.css";
 
 function App() {
   const [imageURL, setImageURL] = useState(null);
-
+  const [imageList, setImageList] = useState(null);
   const sigCanvas = useRef({});
 
   const clear = () => sigCanvas.current.clear();
@@ -20,44 +20,57 @@ function App() {
     });
   };
 
-  return (
-    <div className="App">
-      <h1>Signature Pad Example</h1>
-      <Popup
-        modal
-        trigger={<button>Open Signature Pad</button>}
-        closeOnDocumentClick={false}
-      >
-        {(close) => (
-          <>
-            <SignaturePad
-              ref={sigCanvas}
-              canvasProps={{
-                className: "signatureCanvas",
-              }}
-            />
-            <button onClick={save}>Save</button>
-            <button onClick={clear}>Clear</button>
-            <button onClick={close}>Close</button>
-          </>
-        )}
-      </Popup>
-      <br />
-      <br />
+  // console.log(imageURL);
 
-      {imageURL ? (
-        <img
-          src={imageURL}
-          alt="my signature"
-          style={{
-            display: "block",
-            margin: "0 auto",
-            border: "1px solid black",
-            width: "150px",
-          }}
-        />
-      ) : null}
-    </div>
+  const viewImages = () => {
+    axios.get("http://localhost:3010/api/signatures").then((response) => {
+      setImageList(response.data.data);
+    });
+  };
+  console.log(imageList);
+
+  return (
+    <>
+      <div className="App">
+        <h1>Signature Pad Example</h1>
+        <button onClick={viewImages}>View Image</button>
+        <Popup
+          modal
+          trigger={<button>Open Signature Pad</button>}
+          closeOnDocumentClick={false}
+        >
+          {(close) => (
+            <>
+              <SignaturePad
+                ref={sigCanvas}
+                canvasProps={{
+                  className: "signatureCanvas",
+                }}
+              />
+              <button onClick={save}>Save</button>
+              <button onClick={clear}>Clear</button>
+              <button onClick={close}>Close</button>
+            </>
+          )}
+        </Popup>
+        <br />
+        <br />
+
+        {imageURL ? (
+          <img
+            src={imageURL}
+            alt="my signature"
+            style={{
+              display: "block",
+              margin: "0 auto",
+              border: "1px solid black",
+              width: "150px",
+            }}
+          />
+        ) : null}
+      </div>
+      <img src={imageList} alt="signature" />
+    </>
   );
 }
 
